34ec7b7dd092080cc1b5c68a22c874f6bbe891f9
theerawuth <theerawuth.p@augmentis.biz>
2016-08-30 10:28:45 +0700

    Start DragAndDraw

---
+++ app/src/main/java/com/augmentis/ayp/dnd/SingleFragmentActivity.java
@@ -0,0 +1,42 @@
+package com.augmentis.ayp.dnd;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "SingleFragmentActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+
+        Log.d(TAG, "On Create Activity");
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment f = fm.findFragmentById(R.id.alone_fragment);
+
+        if(f == null) {
+            f = onCreateFragment();
+
+            fm.beginTransaction()
+                    .add(R.id.alone_fragment, f)
+                    .commit();
+            Log.d(TAG, "Fragment is created");
+        }
+        else
+        {
+            Log.d(TAG, "Fragment have already been created");
+        }
+    }
+
+    protected abstract Fragment onCreateFragment();
+
+    }
+
